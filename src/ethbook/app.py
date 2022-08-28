c2834eca672a6385a8436105de3189de27ac8d08
Alex San <goodalexhunting@gmail.com>
2022-08-28 23:26:14 +0100

    🪙Binance OrderBook prototype 🪙
    Co-authored-by: prest <prestance1@users.noreply.github.com>

--- src/ethbook/app.py
+++ src/ethbook/app.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from websocket import WebSocketApp, enableTrace
 import json
 import requests
@@ -6,40 +7,88 @@ from tabulate import tabulate
 # https://binance-docs.github.io/apidocs/spot/en/#live-subscribing-unsubscribing-to-streams
 
 
-def on_message(ws, message) -> None:
-    print("received a message")
+class BinanceOrderBook():
+    
+    def __init__(self, limit = 20, delay = '1000ms') -> BinanceOrderBook:
+        self.bids = SortedDict()
+        self.asks = SortedDict()
+        self.last_update_id:int = None 
+        self.pair = "btcusdt"
+        self.orderbook_url = f"https://api.binance.com/api/v3/depth?symbol={self.pair.upper()}&limit={limit}"
+        self.ws_url = f"wss://stream.binance.com:9443/ws/{self.pair}@depth{limit}@{delay}"
+    
+    def _wrap_callback(self,f):
+        def wrapped_f(ws, *args, **kwargs):
+            try:
+                f(ws, *args, **kwargs)
+            except Exception as e:
+                raise Exception(f"Error running websocket callback: {e}")
+            
+        return wrapped_f
 
-def on_open(ws, message) -> None:
-    print("Opening conection")
+
+    def _on_message(self, ws, message) -> None:
+        print("received new message")
+        message = json.loads(message)
+
+        if message['lastUpdateId'] < self.last_update_id:
+            print(f"{message['lastUpdateId']=} {self.last_update_id=}")
+            return
+        # print(message["bids"])
+
+        for price_level, new_quantity in message['bids']:
+            #print("iterating loop1")
+            price_level = float(price_level)
+
+            self.bids[price_level] = float(new_quantity)
+            if self.bids[price_level] == float(0):
+                print(f"PRICE LEVEL DELETED: {price_level}")
+                self.bids.pop(price_level)
+
+        for price_level, new_quantity in message['asks']:
+            #print("iterating loop2")
+            
+            price_level = float(price_level)
+            self.asks[price_level] = float(new_quantity)
+            if self.asks[price_level] == float(0):
+                print(f"PRICE LEVEL DELETED: {price_level}")
+                self.asks.pop(price_level)
+        
+        self.last_update_id = message['lastUpdateId']
+        
+        table_1 = tabulate(self.bids.items()[:5], headers=['Price', 'Quantity'], tablefmt='orgtbl')
+        table_2 = tabulate(self.asks.items()[:5], headers=['Price', 'Quantity'], tablefmt='orgtbl')
+
+        print(f"{table_1}{table_2}")
+
+    def _on_open(self, ws, message) -> None:
+        print("Opening conection")
+
+
+    def run(self) -> None:
+
+        orderbook = requests.get(self.orderbook_url).json()
+        
+        print("Orderbook fetched")
+
+        for price_level in orderbook["bids"]:
+            self.bids[float(price_level[0])] = float(price_level[1])
+        
+        for price_level in orderbook["asks"]:
+            self.asks[float(price_level[0])] = float(price_level[1])
+        self.last_update_id = orderbook["lastUpdateId"]
+        print("Orderbook populated")
+        ws2 = WebSocketApp(self.ws_url, on_message=self._wrap_callback(self._on_message), on_open=self._wrap_callback(self._on_message))
+        ws2.run_forever()
 
 
 def main() -> None:
-    # enableTrace(True)
-    # ws =  WebSocketApp("wss://api.gemini.com/v1/marketdata/BTCUSD", on_message=on_message, on_open=on_open)
-    # ws.run_forever()
+    binance_client: BinanceOrderBook = BinanceOrderBook()
+    binance_client.run()
 
-    ws2 =  WebSocketApp("wss://stream.binance.com:9443/ws/bnbbtc@depth20", on_message=on_message, on_open=on_open)
-    # ws2.run_forever()
-    bids: SortedDict = SortedDict()
-    asks: SortedDict = SortedDict()
-    orderbook = requests.get("https://api.binance.com/api/v3/depth?symbol=BNBBTC&limit=20").json()
-    
-    print(orderbook["lastUpdateId"])
-    
 
-    for price_level in orderbook["bids"]:
-        bids[float(price_level[0])] = float(price_level[1])
-    
-    for price_level in orderbook["asks"]:
-        asks[float(price_level[0])] = float(price_level[1])
-
-    
-
-    
-
-    print(tabulate(bids.items()[:], headers=['Price', 'Quantity'], tablefmt='orgtbl'))
-    
-
+if __name__ == "__main__":
+    main()
 
 #TODO
 #1) get current state of orderbook, and store it in two sorted containers
@@ -55,13 +104,13 @@ def main() -> None:
 #   "s": "BNBBTC",      // Symbol
 #   "U": 157,           // First update ID in event
 #   "u": 160,           // Final update ID in event
-#   "b": [              // Bids to be updated
+#   "b": [              // seBids to be updated
 #     [
 #       "0.0024",       // Price level to be updated
 #       "10"            // Quantity
 #     ]
 #   ],
-#   "a": [              // Asks to be updated
+#   "a": [              // self.Asks to be updated
 #     [
 #       "0.0026",       // Price level to be updated
 #       "100"           // Quantity
@@ -70,7 +119,6 @@ def main() -> None:
 # }
     
 
-if __name__ == "__main__":
-    main()    
+
 
 
